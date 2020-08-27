f15ff5f09b9c9fe9cff66f25b8aa82095c825d87
david reinfurt <reinfurt@o-r-g.com>
2020-08-27 17:01:06 -0400

    mv views/pdf --> views/read

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ $uri = explode('/', $_SERVER['REQUEST_URI']);
 require_once("views/head.php");
 if ($uri[1] == 'print')
     require_once("views/print.php");
-else if ($uri[1] == 'pdf')
-    require_once("views/pdf.php");
+else if ($uri[1] == 'read')
+    require_once("views/read.php");
 else 
     require_once("views/main.php");
 require_once("views/badge.php");
