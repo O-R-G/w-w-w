cef9332584f9011b81e421e216e1f5bae1a12f08
david reinfurt <reinfurt@o-r-g.com>
2024-04-05 17:24:11 -0400

    ibid

--- static/js/menu.js
+++ static/js/menu.js
@@ -2,8 +2,8 @@
 
 var menu = document.getElementById('menu');
 var main = document.getElementById('main');
-var badge = document.getElementById('badge');
-badge.addEventListener('click', hide_show_menu);
+var badge_ = document.getElementById('badge');
+badge_.addEventListener('click', hide_show_menu);
 
 function hide_show_menu() {
     menu.classList.toggle("hidden");
