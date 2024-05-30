433e3b29d44b99e22f29daf00111b0109b288606
wei <wei@o-r-g.com>
2024-05-30 21:28:28 +0800

    pop last item of  if it is an empty string

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $request = $_SERVER['REQUEST_URI'];
 $requestclean = strtok($request,"?");
 $uri = explode('/', $requestclean);
+if(empty(end($uri))) array_pop($uri);
 
 require_once("views/head.php");
 if ($uri[1] == 'print')
